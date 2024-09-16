0e79c9601b79a0341b1f723340ab4cb6e27937f5
Andreas Van Loon <andreas.vanloon@student.kdg.be~>
2024-09-16 12:33:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Groeiproject Programmeren 2 - Java
+* Naam: Andreas Van Loon
+* Klasgroep: TI-Flex
+* Studentennummer:
+* Academiejaar 2024-2025
+* 
---
+++ m1_herhaling/src/main/java/be/kdg/java2/Demo1.java
@@ -0,0 +1,8 @@
+package be.kdg.java2;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
